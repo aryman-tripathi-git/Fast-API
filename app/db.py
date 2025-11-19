dd468165238a617ac54b7690875d59c2c6a213f7
Aryman Tripathi <arymantripathi@arymantripathi.local>
2025-11-19 18:59:18 +0530

    updated the db code for latest sqlalchemy version

--- app/db.py
+++ app/db.py
@@ -1,31 +1,44 @@
-from collections.abc import AsyncGenerator
 import uuid
+from datetime import datetime
+from collections.abc import AsyncGenerator
 
-from sqlalchemy import column, String, text, DateTime, ForeignKey
+from sqlalchemy import String, Text, DateTime
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
-from sqlalchemy.orm import DeclarativeBase, relationship
-import datetime
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import text
+
 
 DATABASE_URL = "sqlite+aiosqlite:///./test.db"
 
-class Post(DeclarativeBase):
+class Base(DeclarativeBase):
+    pass
+
+class Post(Base):
     __tablename__ = "posts"
     
-    id = column(UUID(as_uuid=True), primary_Key= True, default=uuid.uuid4)
-    caption = column(text)
-    url = column(String, nullable = False)
-    file_type = column(String, nullable = False)
-    file_name = column(String, nullable = False)
-    created_at = column(DateTime, default = datetime.utcnow)
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), 
+        primary_key=True, 
+        default=uuid.uuid4
+    )  
+    caption: Mapped[str | None] = mapped_column(Text, nullable=True) 
+    url: Mapped[str] = mapped_column(String, nullable=False)
+    file_type: Mapped[str] = mapped_column(String, nullable=False)
+    file_name: Mapped[str] = mapped_column(String, nullable=False)
+    
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
 
 engine = create_async_engine(DATABASE_URL)
-async_session_maker = async_sessionmaker(engine,expire_on_commit=False)
+
+async_session_maker = async_sessionmaker[AsyncSession](engine, expire_on_commit=False)
 
 async def create_db_and_tables():
+    """Initializes the database and creates all defined tables."""
     async with engine.begin() as conn:
-        await conn.run_sync(DeclarativeBase.metadata.create_all)
+        await conn.run_sync(Base.metadata.create_all)
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    """Provides an asynchronous database session using Dependency Injection."""
     async with async_session_maker() as session:
         yield session
